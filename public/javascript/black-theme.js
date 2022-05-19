292094064d1a8d086ac9874796a264d9f0e41b67
Prasanna <prasannavelmurugan@gmail.com>
2022-05-20 00:27:56 +0530

    rv

--- public/javascript/black-theme.js
+++ public/javascript/black-theme.js
@@ -7,5 +7,10 @@ toggleDarkMode.addEventListener("click", () => {
 const darkmode = () => {
     document.querySelector("body").classList.toggle("dark-mode");
     document.querySelector(".navbar").classList.toggle("dark-mode");
+    document.querySelector(".logo").classList.toggle("dark-mode");
+    document.querySelectorAll(".nav_atag").forEach(element => {
+        element.classList.toggle("dark-mode");
+    });
+
     toggleDarkMode.classList.toggle("dark-mode");
 };
